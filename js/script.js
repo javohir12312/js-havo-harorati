d570e6c3f633d2568cdc287a834e4170896757ec
javohir12312 <abduxalilovjavohir393@gmail.com>
2022-08-08 15:19:45 +0300

    full

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let elForm = document.querySelector(".form");
 let enterText = document.querySelector(".enter_text");
 let elCheckbox = document.querySelector(".checkbox");
+let elZalCheckbox = document.querySelector(".checkbox_zal")
 
 let elP = document.querySelector(".result");
 
@@ -9,6 +10,9 @@ elForm.addEventListener("submit", function(evt){
 
   if (elCheckbox.checked == 1){
     elP.textContent = "Yomg'ir yog'moqda havo yugurish uchun noqulay"
+  }
+  else if(elZalCheckbox.checked == 1) {
+    elP.textContent = "Zalga borsa boladi"
   }
   else if(enterText.value < 5 || enterText.value > 40) {
     elP.textContent = "Havo juda noqulay"
